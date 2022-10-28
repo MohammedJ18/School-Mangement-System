8a8f57f447df560e681f8b243da39f1f49a6f6ee
Mohammed Jawad <jawad6686@gmail.com>
2022-10-28 20:08:25 +0300

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'person' => [
+            'driver' => 'session',
+            'provider' => 'people',
+        ],
     ],
 
     /*
@@ -63,6 +67,10 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+        'people' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Person::class,
         ],
 
         // 'users' => [
