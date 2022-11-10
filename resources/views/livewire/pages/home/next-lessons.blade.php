8db1e6b8315b1a6a828b82b69888f1ce8dc0f320
Mosa Nameer <mosa.mn777@gmail.com>
2022-11-10 15:54:32 +0300

    add

--- resources/views/livewire/pages/home/next-lessons.blade.php
+++ resources/views/livewire/pages/home/next-lessons.blade.php
@@ -1,83 +1,21 @@
 <div>
-    <p class="font-bold text-lg w-full">ألمحاضرات القادمة</p>
-    <div class="border-2 mt-3 h-auto ">
-        <div class="mt-6 p-3">
-            <div class="flex flex-col mt-1">
+    <p class="font-bold text-lg w-full 2xl:text-2xl 2xl:w-auto">ألمحاضرات القادمة</p>
+    <div class="border-2 mt-4 h-auto ">
+        <div class="mt-4 p-3">
+            @for ($i = 0; $i < 7; $i++)
+            <div class="flex flex-col mt-2 2xl:mt-5">
                 <div class="flex">
-                    <p class="font-bold text-2xl mt-3 ml-2">08:30</p>
+                    <p class="font-bold text-2xl mt-3 ml-2 mr-2 2xl:text-4xl 2xl:mr-4">08:30</p>
                     <div>
-                        <p class="font-semibold text-lg">كيمياء</p>
-                        <p class="text-xs text-secondary-300"> السادس العلمي</p>
+                        <p class="font-semibold text-lg 2xl:text-xl">كيمياء</p>
+                        <p class="text-xs text-secondary-300 2xl:text-base"> السادس العلمي</p>
                     </div>
                 </div>
                 <div class=" h-1 w-auto mt-2 bg-red-500"></div>
             </div>
+            @endfor
 
-            <div class="flex flex-col mt-1">
-                <div class="flex">
-                    <p class="font-bold text-2xl mt-3 ml-2">08:30</p>
-                    <div>
-                        <p class="font-semibold text-lg">كيمياء</p>
-                        <p class="text-xs text-secondary-300"> السادس العلمي</p>
-                    </div>
-                </div>
-                <div class=" h-1 w-auto mt-2 bg-purple-500"></div>
-            </div>
 
-            <div class="flex flex-col mt-1">
-                <div class="flex">
-                    <p class="font-bold text-2xl mt-3 ml-2">08:30</p>
-                    <div>
-                        <p class="font-semibold text-lg">كيمياء</p>
-                        <p class="text-xs text-secondary-300"> السادس العلمي</p>
-                    </div>
-                </div>
-                <div class=" h-1 w-auto mt-2 bg-green-500"></div>
-            </div>
-
-            <div class="flex flex-col mt-1">
-                <div class="flex">
-                    <p class="font-bold text-2xl mt-3 ml-2">08:30</p>
-                    <div>
-                        <p class="font-semibold text-lg">كيمياء</p>
-                        <p class="text-xs text-secondary-300"> السادس العلمي</p>
-                    </div>
-                </div>
-                <div class=" h-1 w-auto mt-2 bg-pink-500"></div>
-            </div>
-
-            <div class="flex flex-col mt-1">
-                <div class="flex">
-                    <p class="font-bold text-2xl mt-3 ml-2">08:30</p>
-                    <div>
-                        <p class="font-semibold text-lg">كيمياء</p>
-                        <p class="text-xs text-secondary-300"> السادس العلمي</p>
-                    </div>
-                </div>
-                <div class=" h-1 w-auto mt-2 bg-gray-500"></div>
-            </div>
-
-            <div class="flex flex-col mt-1">
-                <div class="flex">
-                    <p class="font-bold text-2xl mt-3 ml-2">08:30</p>
-                    <div>
-                        <p class="font-semibold text-lg">كيمياء</p>
-                        <p class="text-xs text-secondary-300"> السادس العلمي</p>
-                    </div>
-                </div>
-                <div class=" h-1 w-auto mt-2 bg-blue-500"></div>
-            </div>
-
-            <div class="flex flex-col mt-1">
-                <div class="flex">
-                    <p class="font-bold text-2xl mt-3 ml-2">08:30</p>
-                    <div>
-                        <p class="font-semibold text-lg">كيمياء</p>
-                        <p class="text-xs text-secondary-300"> السادس العلمي</p>
-                    </div>
-                </div>
-                <div class=" h-1 w-auto mt-2 bg-yellow-500"></div>
-            </div>
         </div>
     </div>
 </div>
---
+++ resources/views/livewire/pages/home/stats-card.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <div>
+        <p class="font-bold text-lg 2xl:text-2xl">أحصائيات سريعة</p>
+    </div>
+    <div class="flex gap-x-5 mt-3 2xl:mt-4">
+        @for ($i = 0; $i < 4; $i++)
+            <div class="border-2 border-secondary-200 w-32 h-32 2xl:w-48 2xl:h-48">
+                <p class="text-center text-4xl font-bold mt-4 2xl:text-6xl 2xl:mt-8">52</p>
+                <p class="text-center mt-5 font-semibold text-base 2xl:text-2xl">غائبا</p>
+            </div>
+        @endfor
+    </div>
+
+
+</div>
