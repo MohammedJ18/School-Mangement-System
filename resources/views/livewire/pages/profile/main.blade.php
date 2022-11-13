d2b91d01e488e00c32294d38a25853adec09159e
Mosa Nameer <mosa.mn777@gmail.com>
2022-11-14 00:36:34 +0300

    add

--- resources/views/livewire/pages/profile/main.blade.php
+++ resources/views/livewire/pages/profile/main.blade.php
@@ -5,13 +5,11 @@
         </div>
         <div class="mr-5 mt-6">
             <p class="text-xl font-semibold">{{ $student->user->name }}</p>
-            <p class="text-base">تاريخ الميلاد : {{ $student->user->birth_date }}</p>
-            <p class="">أولياء الامور:
-                {{-- @foreach($student->parents As $parent)
-                <lu>
-                    <li><a href="#">{{$parent->user->name}}</a></li>
-                </lu>
-                @endforeach --}}
+            <p class="text-base font-semibold">تاريخ الميلاد : {{ $student->user->birth_date }}</p>
+            <p class="font-semibold">أولياء الامور:
+                @foreach($student->parents As $parent)
+                    <a href="#">{{$parent->user->name}}</a>
+                @endforeach
             </p>
         </div>
     </div>
