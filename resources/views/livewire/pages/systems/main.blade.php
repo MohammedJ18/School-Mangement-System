9d3ed5955a8315542d4fbc7ef518734c48b1724a
Mosa Nameer <mosa.mn777@gmail.com>
2022-11-30 11:07:05 +0300

    add

--- resources/views/livewire/pages/systems/main.blade.php
+++ resources/views/livewire/pages/systems/main.blade.php
@@ -32,71 +32,8 @@
 
     <div class="grid grid-cols-12 w-full mt-10">
         <div class="col-span-4">
-            <div x-data="{ modelOpen: false }">
-                <button @click="modelOpen =!modelOpen"
-                    class="border-2 border-secondary-200 w-full h-28 items-center flex justify-center">
-                    <svg width="32" height="32" viewBox="0 0 24 24">
-                        <path fill="#888888" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z" />
-                    </svg>
-                </button>
 
-                <div x-show="modelOpen" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title"
-                    role="dialog" aria-modal="true">
-                    <div
-                        class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
-                        <div x-cloak @click="modelOpen = false" x-show="modelOpen"
-                            x-transition:enter="transition ease-out duration-300 transform"
-                            x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
-                            x-transition:leave="transition ease-in duration-200 transform"
-                            x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-                            class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true"></div>
-
-                        <div x-cloak x-show="modelOpen" x-transition:enter="transition ease-out duration-300 transform"
-                            x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                            x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-                            x-transition:leave="transition ease-in duration-200 transform"
-                            x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-                            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                            class="inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl">
-                            <div class="flex items-center justify-between space-x-4">
-                                <h1 class="text-xl font-medium text-gray-800 ">اضافة نظام</h1>
-
-                                <button @click="modelOpen = false"
-                                    class="text-gray-600 focus:outline-none hover:text-gray-700">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                    </svg>
-                                </button>
-                            </div>
-
-                            <div>
-                                <div class="mt-4">
-                                    <label class="block text-sm font-medium text-gray-700 text-right">اسم
-                                        المدرسة</label>
-                                    <div class="mt-1">
-                                        <input type="text" name="name" id="name"
-                                            class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
-                                            placeholder="المدرسة">
-                                    </div>
-                                </div>
-                                <div class="mt-4">
-                                    <label class="block text-sm font-medium text-gray-700 text-right">الموقع</label>
-                                    <div class="mt-1">
-                                        <input type="text" name="name" id="name"
-                                            class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
-                                            placeholder="الموقع">
-                                    </div>
-                                </div>
-                                <button type="button"
-                                    class="text-white bg-primary-100 font-medium rounded-lg w-full text-sm px-5 py-2.5 mr-2 mb-2 mt-6">أضافة</button>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @include('livewire.pages.systems.add-system')
             <div>
                 <a href="#">
                     @for ($i = 0; $i < 3; $i++)
@@ -115,5 +52,4 @@
         </div>
     </div>
     <!-- component -->
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </div>
