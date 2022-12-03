c277183685983784d10aeab5615d5deb94972882
Mohammed Jawad <jawad6686@gmail.com>
2022-12-03 12:44:23 +0300

    add

--- app/Http/Livewire/Pages/Home/Main.php
+++ app/Http/Livewire/Pages/Home/Main.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Livewire\Pages\Home;
 
+use App\Models\Student;
+use App\Models\Teacher;
 use Livewire\Component;
 
 class Main extends Component
 {
+    public $counts = [];
     public function render()
     {
+        $this->counts = [
+            [
+                'name' => 'الطلاب',
+                'count' => Student::count(),
+            ],
+            [
+                'name' => 'الأساتذة',
+                'count' => Teacher::count(),
+            ],
+        ];
+
         return view('livewire.pages.home.main');
     }
 }
--- app/Http/Livewire/Pages/Home/StatsCard.php
+++ app/Http/Livewire/Pages/Home/StatsCard.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 
 class StatsCard extends Component
 {
+    public $item;
     public function render()
     {
         return view('livewire.pages.home.stats-card');
