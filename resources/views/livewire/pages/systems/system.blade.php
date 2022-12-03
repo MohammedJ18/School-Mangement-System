0caae3424e6c730e3b7ac5f56924327dcc547b83
Mosa Nameer <mosa.mn777@gmail.com>
2022-12-03 17:11:22 +0300

    add

--- resources/views/livewire/pages/systems/system.blade.php
+++ resources/views/livewire/pages/systems/system.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="border-2 border-secondary-200 w-80 h-28 mt-4 p-3">
+    <div class="border-2 border-secondary-200 w-80 h-28 mt-4 p-3 hover:scale-110 duration-300">
         <p class="font-bold text-lg">اعدادية شط العرب</p>
         <p class=" text-secondary-500">البصرة-التنومة</p>
         <p class=" text-secondary-500">الصفوف - 12</p>
