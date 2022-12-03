986d305bef8b883ea8b29da22784df10f5f1f841
Mosa Nameer <mosa.mn777@gmail.com>
2022-12-03 19:36:45 +0300

    add

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,23 +5,23 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
         <title>{{ config('app.name', 'Laravel') }}</title>
-    
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Tajawal&display=swap" rel="stylesheet">
         <script src="https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js"></script>
-    
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
     </head>
 
-<body>
-    <div class="font-sans text-gray-900 antialiased bg-white">
+<body dir="rtl">
+    <div class="font-Tajawal text-gray-900 antialiased bg-white">
         {{ $slot }}
     </div>
     @livewireScripts
--- resources/views/livewire/guest/home/main.blade.php
+++ resources/views/livewire/guest/home/main.blade.php
@@ -1,3 +1,17 @@
-<div>
-    The Master doesn't talk, he acts.
+<div class="">
+    <div class="flex justify-between my-5 mx-10">
+        <button class="border-2 border-primary-100 w-40 h-10">
+            <span class="">
+                تواصل معنا
+            </span>
+        </button>
+        <p class="text-xl text-primary-100">مدرستي</p>
+    </div>
+
+    <div class="text-center mt-24">
+        <p class="text-3xl font-semibold">
+            أدارة وتنظيم المؤسسات التعليمية
+        </p>
+    </div>
+
 </div>
