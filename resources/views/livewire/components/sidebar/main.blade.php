73cf0ecda46099499f7cf3e97d74778cab71e3cf
Mohammed Jawad <jawad6686@gmail.com>
2022-12-03 17:59:18 +0300

    Update main.blade.php

--- resources/views/livewire/components/sidebar/main.blade.php
+++ resources/views/livewire/components/sidebar/main.blade.php
@@ -3,7 +3,7 @@
         <div class="flex flex-col justify-between py-4 px-3 h-full bg-secondary-100 sticky">
             <ul class=" space-y-4 mt-3">
                 <li>
-                    <a href="{{ route('home') }}" class="flex items-center self-center p-2 hover:bg-primary-100 ">
+                    <a href="{{ route('systems') }}" class="flex items-center self-center p-2 hover:bg-primary-100 ">
                         <svg width="32" height="24" viewBox="0 0 24 24">
                             <path fill="#676e7c" d="M4 21V9l8-6l8 6v12h-6v-7h-4v7Z" />
                         </svg> </a>
