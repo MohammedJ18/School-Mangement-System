28b43d9447141168bc8ed25e8e3363fba22cc225
Mosa Nameer <mosa.mn777@gmail.com>
2022-12-31 12:19:20 +0300

    add

---
+++ app/Http/Livewire/Pages/Schedules/AddSchedule.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Schedules;
+
+use Livewire\Component;
+
+class AddSchedule extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.schedules.add-schedule');
+    }
+}
---
+++ app/Http/Livewire/Pages/Schedules/EditSchedule.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Schedules;
+
+use Livewire\Component;
+
+class EditSchedule extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.schedules.edit-schedule');
+    }
+}
